fd4f695bbfc46e63eda0a6b3ac1f6ff0e7853386
nesa402610 <argyleferris@gmail.com>
2022-06-06 16:48:41 +0300

    сам проект

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import imageD from './images/image-header-desktop.jpg';
+import imageM from './images/image-header-mobile.jpg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const stats = [
+        {heading: '10k+', body: 'companies'},
+        {heading: '314', body: 'templates'},
+        {heading: '12M+', body: 'queries'},
+    ];
+    return (
+        <div className="container">
+            <div className={'content'}>
+                <div className={'infoBlock'}>
+                    <div className={'mainInfo'}>
+                        <h1>Get <span>insight</span> that help your business grow.</h1>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dicta dolores
+                           exercitationem facere necessitatibus quas reiciendis sapiente soluta totam voluptates! Cumque
+                           explicabo facilis molestiae neque optio recusandae reprehenderit unde vitae?</p>
+                    </div>
+                    <div className={'statsBlock'}>
+                        {stats.map(stat =>
+                            <div className={'statsItem'}>
+                                <div className={'statsItem__head'}>
+                                    {stat.heading}
+                                </div>
+                                <div className={'statsItem__body'}>
+                                    {stat.body}
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                <div className={'imageBlock'} style={{backgroundImage: `url(${imageD})`}}>
+                    <img src={imageD} alt=""/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
